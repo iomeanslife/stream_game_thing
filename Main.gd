6278162e4dafda3f72f6aff600259726bc6bb9c6
iomeanslife <omar@ajerray.de>
2024-09-26 14:32:22 +0100

    Milestone manual assault

--- Main.gd
+++ Main.gd
@@ -12,11 +12,13 @@ var assaultRound: int = 0
 @onready var enemyLocation = $EnemyLocation
 @onready var astroEggLocation = $AstroEggLocation
 @onready var roundLabel = $SubViewportContainer/SubViewport/HBoxContainer/RoundCounter
+@onready var hBoxContainer = $SubViewportContainer/SubViewport/HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT,false)
 	get_tree().get_root().set_transparent_background(false)
+	hBoxContainer.modulate.a = 0.5
 	
 	# TODO: fill viewer from twitch api, only followers.
 	for n in 20:
@@ -45,7 +47,7 @@ func _on_check_button_toggled(button_pressed):
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT,!showStuff)
 	get_tree().get_root().set_transparent_background(!showStuff)
 
-func _on_button_pressed():	
+func _on_button_pressed():
 	if currentViewerIndex < viewers.size():
 		viewers[currentViewerIndex].visible = false
 	assaultRound += 1
@@ -62,3 +64,14 @@ func _on_button_pressed():
 	if currentViewerIndex < viewers.size():
 		viewers[currentViewerIndex].visible = true
 		viewers[currentViewerIndex].attack(currentEnemy)
+
+
+
+func _on_h_box_container_mouse_entered():
+	hBoxContainer.modulate.a = 1
+	pass # Replace with function body.
+
+
+
+func _on_h_box_container_mouse_exited():
+	hBoxContainer.modulate.a = 0.5
